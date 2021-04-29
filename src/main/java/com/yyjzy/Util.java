3fc0916b8222068a4dc4b29fd722897a6a748ffe
yyjgit66 <18392562566@163.com>
2021-04-29 09:57:07 +0800

    first commit

---
+++ src/main/java/com/yyjzy/Util.java
@@ -0,0 +1,17 @@
+package com.yyjzy;
+
+public class Util {
+
+    public static String getCapital(String str){
+        // 转为char数组
+        char[] ch = str.toCharArray();
+        // 得到大写字母
+        for(int i = 0; i < ch.length ; i++){
+            if(ch[i] >= 'A' && ch[i] <= 'Z'){
+                return String.valueOf(ch[i]);
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/yyjzy/sql/GenerateSql.java
@@ -0,0 +1,99 @@
+package com.yyjzy.sql;
+
+import com.yyjzy.Util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 生成sql
+ * @author yyj
+ */
+public class GenerateSql {
+
+    /**
+     * 获取插入的sql语句
+     * @param valueType
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static String  getInserting(Object valueType) throws IllegalAccessException {
+        Field[] declaredFields = valueType.getClass().getDeclaredFields();
+        String packageName = valueType.getClass().getName();
+        int lastIndex = packageName.lastIndexOf(".");
+        String className = packageName.substring(lastIndex + 1);
+        String  tableName = getField(className);
+        StringBuilder insertName = new StringBuilder();
+            StringBuilder insertValue = new StringBuilder();
+            for (Field declaredField : declaredFields) {
+                declaredField.setAccessible(true);
+                Object value = declaredField.get(valueType);
+                if(null!=value && !value.equals("")){
+                    insertValue.append("'"+value+"'");
+                    insertValue.append(",");
+                    String name = declaredField.getName();
+                      name = getField(name);
+
+                    insertName.append(name);
+                    insertName.append(",");
+                }
+
+            return  "insert into "+ tableName + " ("+ insertName.substring(0,insertName.length()-1)+")"+ " values ("+insertValue.substring(0,insertValue.length()-1)+");";
+        }
+        return null;
+    }
+
+    /**
+     * 获取大写字母
+     * @param values
+     * @return
+     * @throws IOException
+     */
+    public static String getTemplate(List<Map<String,String>> values) throws IOException {
+        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("yyjzy.text");
+        byte b[] = new byte[1024];
+        int len = 0;
+        /**
+         * 所有读取的内容都使用temp接收
+         */
+        int temp=0;
+        /**
+         *  当没有读取完时，继续读取
+         */
+        while((temp=inputStream.read())!=-1){
+            b[len]=(byte)temp;
+            len++;
+        }
+        inputStream.close();
+        String result = new String(b, 0, len);
+        for (Map<String, String> value : values) {
+            Set<String> key = value.keySet();
+            for (String mapName : key) {
+                result= result.replace("${"+mapName+"}",value.get(mapName));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 获取数据库字段
+     * @param name
+     * @return
+     */
+    public static String getField(String name){
+        String capital = Util.getCapital(name);
+        if(null!=capital && !"".equals(capital)){
+            int index = name.indexOf(capital);
+            String prefix = name.substring(0, index);
+            String suffix = name.substring(index + 1);
+            String substring = name.substring(index, index + 1);
+            suffix = substring.toLowerCase()+suffix;
+            return prefix+"_"+suffix;
+        }
+        return name;
+    }
+}
