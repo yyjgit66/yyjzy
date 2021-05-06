7650000e94623916b00c2ad55e805621f152ae7a
yyjgit66 <18392562566@163.com>
2021-05-06 16:11:08 +0800

    解析excel

---
+++ src/main/java/com/yyjzy/constants/YyjConstants.java
@@ -0,0 +1,12 @@
+package com.yyjzy.constants;
+
+/**
+ * 常量池
+ */
+public interface YyjConstants {
+
+    String XLS = "xls";
+
+    String XLSX = "xlsx";
+
+}
--- src/main/java/com/yyjzy/sql/GenerateSql.java
+++ src/main/java/com/yyjzy/sql/GenerateSql.java
@@ -1,20 +1,21 @@
 package com.yyjzy.sql;
 
 
+import com.yyjzy.constants.YyjConstants;
 import com.yyjzy.utils.YyjUtil;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -112,33 +113,85 @@ public class GenerateSql {
         return name;
     }
 
-    public void temp(File file){
-        //需要解析的Excel文件
-        //File file=new  File("e:/sheet2.xls");
+
+    /**
+     * 解析Excel为insert语句
+     * @param file
+     * @return
+     */
+    public static List<String>  analysisOfExcelForInsert(File file){
         try {
-            //获取工作簿
-            FileInputStream fs= FileUtils.openInputStream(file);
-            HSSFWorkbook workbook=new HSSFWorkbook(fs);
+            if (!file.isFile() || !file.exists()) {
+                return null;
+            }
+            //.是特殊字符，需要转义！！！！！
+            String[] split = file.getName().split("\\.");
+            Workbook workbook;
+            if (YyjConstants.XLS.equals(split[1])){
+                //文件流对象
+                FileInputStream fis = new FileInputStream(file);
+                workbook = new HSSFWorkbook(fis);
+            }else if (YyjConstants.XLSX.equals(split[1])){
+                workbook = new XSSFWorkbook(file);
+            }else {
+                System.out.println("文件类型错误!");
+                return null;
+            }
             //获取第一个工作表
-            HSSFSheet hs=workbook.getSheetAt(0);
+            Sheet hs=workbook.getSheetAt(0);
             //获取Sheet的第一个行号和最后一个行号
             int last=hs.getLastRowNum();
             int first=hs.getFirstRowNum();
             //遍历获取单元格里的信息
-            for (int i = first; i <last; i++) {
-                HSSFRow row=hs.getRow(i);
-                int firstCellNum=row.getFirstCellNum();//获取所在行的第一个行号
-                int lastCellNum=row.getLastCellNum();//获取所在行的最后一个行号
+            String fields = "";
+            String values = "";
+            List<String> sqlList = new ArrayList<>();
+            for (int i = first; i <=last; i++) {
+                Row row=hs.getRow(i);
+                //获取所在行的第一个行号
+                int firstCellNum=row.getFirstCellNum();
+                //获取所在行的最后一个行号
+                int lastCellNum=row.getLastCellNum();
                 for (int j = firstCellNum; j <lastCellNum; j++) {
-                    HSSFCell cell=row.getCell(j);
-                    String value=cell.getStringCellValue();
-                    System.out.print(value+" ");
+                    Cell cell=row.getCell(j);
+                    String value=getString(cell);
+                    if(i==0){
+                        fields+=value+",";
+                    }else{
+                        values+=value+",";
+                    }
                 }
-                System.out.println();
+                if(!"".equals(fields)&&!"".equals(values)){
+                    String sql = "insert into "+split[0]+"("+fields.substring(0,fields.length()-1)+") values("+values.substring(0,values.length()-1)+");";
+                    sqlList.add(sql);
+                    values="";
+                }
+
             }
-        } catch (IOException e) {
+            return sqlList;
+        } catch (IOException | InvalidFormatException e) {
             log.info("读取文件异常"+e.getMessage());
         }
+      return null;
 
+    }
+
+    /**
+     * 把单元格的内容转为字符串
+     *
+     * @param cell 单元格
+     * @return String
+     */
+    public static String getString(Cell cell) {
+        if (cell == null) {
+            return "";
+        }
+        if (cell.getCellType() == CellType.NUMERIC) {
+            return String.valueOf(cell.getNumericCellValue());
+        } else if (cell.getCellType() == CellType.BOOLEAN) {
+            return String.valueOf(cell.getBooleanCellValue());
+        } else {
+            return cell.getStringCellValue();
+        }
     }
 }
