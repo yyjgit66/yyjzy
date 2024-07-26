83764d982e1c051aaa5117eaa60853addbdc893f
liyilong <elong490@qq.com>
2024-07-26 11:31:46 +0800

    增加数组工具类

---
+++ src/main/java/com/yyjzy/utils/ArrayUtil.java
@@ -0,0 +1,29 @@
+package com.yyjzy.utils;
+
+/**
+ * 数组工具类
+ */
+public class ArrayUtil {
+
+    /**
+     * 数组是否为空
+     *
+     * @param <T>   数组元素类型
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static <T> boolean isEmpty(T[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param <T>   数组元素类型
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static <T> boolean isNotEmpty(T[] array) {
+        return (null != array && array.length != 0);
+    }
+}
